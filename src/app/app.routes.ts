de7099eb5187fe5e86931eac39bafdd9c93e5768
Miguel de la Garza <hola@migueldelagarza.dev>
2023-07-21 15:27:41 -0600

    se agrega redireccionamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 
 export const routes: Routes = [
-  {path: 'agregar', component: AddEmployeeComponent }
+  { path: '', redirectTo: '/agregar', pathMatch: 'full' },
+  { path: 'agregar', component: AddEmployeeComponent }
 ];
